93f0f2d37890f1c9123b5fe3b52cd882bfa20403
laurens <laurens@loots-it.be>
2020-10-28 01:48:34 +0100

    Deleted SimpleNotification and used TestNotification in test instead

--- tests/Channel/ExternalMailTemplateChannelTest.php
+++ tests/Channel/ExternalMailTemplateChannelTest.php
@@ -3,12 +3,13 @@
 namespace LootsIt\LaravelMailTemplateChannel\Tests\Channel;
 
 
+use Illuminate\Support\Facades\Config;
 use Lootsit\LaravelMailTemplateChannel\Drivers\MailTemplateDriver;
 use LootsIt\LaravelMailTemplateChannel\ExternalMailTemplateChannel;
 
 use App\Models\User;
 use LootsIt\LaravelMailTemplateChannel\MailTemplateMessage;
-use LootsIt\LaravelMailTemplateChannel\Tests\SimpleNotification;
+use LootsIt\LaravelMailTemplateChannel\TestNotification;
 use Mockery;
 use Tests\TestCase;
 
@@ -17,15 +18,23 @@ class ExternalMailTemplateChannelTest extends TestCase
 {
     public function testChannel()
     {
+        $fromEmail = 'test@test.com';
+        $fromName = 'test';
+
+        Config::set('external_template_mail.from.email', $fromEmail);
+        Config::set('external_template_mail.from.name', $fromName);
+
         $user = User::factory()->make();
-        $notification = new SimpleNotification();
+        $notification = new TestNotification(1);
+
         $message = new MailTemplateMessage(1, []);
-        $message->subject = "Verify email address";
-        $message->fromEmail = getenv('MAILJET_FROM_EMAILADDRESS');
-        $message->fromName = getenv('MAILJET_FROM_NAME');
-        $message->replyToEmail = getenv('MAILJET_FROM_EMAILADDRESS');
-        $message->replyToName = getenv('MAILJET_FROM_NAME');
+        $message->subject = "Test mail";
+        $message->fromEmail = $fromEmail;
+        $message->fromName = $fromName;
+        $message->replyToEmail = $fromEmail;
+        $message->replyToName = $fromName;
         $message->to = [["Email" => $user->email]];
+        $message->testMessage = true;
 
         $fakeDriver = Mockery::mock(MailTemplateDriver::class);
         $fakeDriver
